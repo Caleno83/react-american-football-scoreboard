bf80f05a45248819c2e83565aac77f8c77446182
Jorge Jimenez <calenocam24@gmail.com>
2020-06-16 22:17:39 -0400

    finished part 3, rendering scores values to the DOM

--- src/App.js
+++ src/App.js
@@ -7,8 +7,10 @@ function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
 
   const number = 0;
+
   const [homeScore, setHomeScore] = useState(number);
   const [awayScore, setAwayScore] = useState(number);
+
 
   return (
     <div className="container">
@@ -18,12 +20,12 @@ function App() {
             <h2 className="home__name">Lions</h2>
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
-            <div className="home__score">32</div>
+  <div className="home__score">{homeScore}</div>
           </div>
           <div className="timer">00:03</div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
-            <div className="away__score">32</div>
+  <div className="away__score">{awayScore}</div>
           </div>
         </div>
         <BottomRow />
